36e31c0186c4d68c09152f052298e76c9edfb753
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 16:46:57 +0530

    Implemented a actuator and swagger

--- src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -1,9 +1,16 @@
 package com.example.customer_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Customer Service API",
+		version = "v1",
+		description = "Customer management APIs for the Order Management System"
+))
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
