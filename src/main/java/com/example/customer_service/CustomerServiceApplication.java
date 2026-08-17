ad6e53a63da22e5615e55f61aa528fb6381ac396
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 19:30:20 +0530

    Implemented a swagger

--- src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.customer_service;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		title = "Customer Service API",
 		version = "v1",
 		description = "Customer management APIs for the Order Management System"
-))
+), servers = @Server(url = "/", description = "Current host"))
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
