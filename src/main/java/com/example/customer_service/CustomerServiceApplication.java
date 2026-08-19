bb68fe2708e611de29763715b388b3fce4c7de9a
Shalini <shalini.m@mitrahsoft.in>
2026-08-19 16:26:05 +0530

    removed url in main application for swagger

--- src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		title = "Customer Service API",
 		version = "v1",
 		description = "Customer management APIs for the Order Management System"
-), servers = @Server(url = "/", description = "Current host"))
+))
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
